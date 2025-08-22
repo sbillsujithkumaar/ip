8873f9ae1204a208aed3b4640ba14c7cb54473d6
sujith <s.bill.sujith.kumaar2@gmail.com>
2025-08-22 13:55:06 +0800

    Leo.java: Change echo to run function that also lists inputs

--- src/main/java/Leo.java
+++ src/main/java/Leo.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Leo {
+    private static final String[] tasksList = new String[100];
+    private static int numOfTask = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,7 +13,7 @@ public class Leo {
         System.out.println("Hello from\n" + logo);
 
         greet();
-        echo();
+        run();
     }
     public static void greet() {
         System.out.println("__________________________________________________________________");
@@ -19,19 +22,39 @@ public class Leo {
         System.out.println("__________________________________________________________________");
     }
 
-    public static void echo() {
+    public static void run() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 exit();
                 break;
+            } else if (input.equals("list")) {
+                printTasksList();
+            } else {
+                addTask(input);
             }
             System.out.println("__________________________________________________________________");
             System.out.println(" " + input);
             System.out.println("__________________________________________________________________");
         }
         scanner.close();
+    }
+
+    public static void addTask(String task) {
+        tasksList[numOfTask] = task;
+        numOfTask++;
+        System.out.println("__________________________________________________________________");
+        System.out.println(" added: " + task);
+        System.out.println("__________________________________________________________________");
+    }
+
+    public static void printTasksList() {
+        System.out.println("__________________________________________________________________");
+        for (int i = 0; i < numOfTask; i++) {
+            System.out.println((i + 1) + ". " + tasksList[i]);
+        }
+        System.out.println("__________________________________________________________________");
     }
 
     public static void exit() {
